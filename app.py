dc7b29bbb0c13868661cba4168593a30a9e6ce3f
sipakem <sipakemsipakem@gmail.com>
2026-05-28 17:45:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ pymysql.install_as_MySQLdb()
 
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
-from flask_mysql import MySQL
+from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 
